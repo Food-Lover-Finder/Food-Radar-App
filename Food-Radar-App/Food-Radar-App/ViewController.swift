3e7b6a18fe5ae4e3b27c16b611c3c4d131773f3f
Brang <brangmai@Brangs-MacBook-Pro.local>
2021-11-04 12:36:12 -0400

    APP inited

---
+++ Food-Radar-App/Food-Radar-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Food-Radar-App
+//
+//  Created by Brang Mai on 11/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
