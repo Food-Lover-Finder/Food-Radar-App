92807a74ec9b606f30b821e487005d8d78591a5b
fatimajavid <fatimajavid101@gmail.com>
2021-11-10 19:12:17 -0500

    commit -Logging in Successfully

--- Food-Radar-App/Food-Radar-App/LoginViewController.swift
+++ Food-Radar-App/Food-Radar-App/LoginViewController.swift
@@ -6,16 +6,27 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 
+    @IBAction func onLogin(_ sender: Any) {
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: )
+    }
+    
     /*
     // MARK: - Navigation
 
