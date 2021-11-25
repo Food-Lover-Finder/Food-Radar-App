eefb4a39576885c0fed6fc11ebbbccfc3093b872
Brang Mai <62917276+brangmai1@users.noreply.github.com>
2021-11-24 22:26:11 -0500

    Update AppDelegate_BACKUP_51328.swift

--- Food-Radar-App/Food-Radar-App/AppDelegate_BACKUP_51328.swift
+++ Food-Radar-App/Food-Radar-App/AppDelegate_BACKUP_51328.swift
@@ -15,19 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-<<<<<<< HEAD
-        Thread.sleep(forTimeInterval: 2.0)
-        return true
-=======
->>>>>>> LoginActivity
 
-        // let parseConfig = ParseClientConfiguration {
-        //         $0.applicationId =  "wTAxU8aAHLsTH4U4MVtB1oTBmkKlku53NMaJm5B1"// <- UPDATE
-        //         $0.clientKey =  "DDcvptPHMaGvWz6jTGSuBMlRXGSG2poVKdIDsre7"// <- UPDATE
-        //         $0.server = "https://parseapi.back4app.com"
-        // }
-        // Parse.initialize(with: parseConfig)
-        //     return true
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId =  "wTAxU8aAHLsTH4U4MVtB1oTBmkKlku53NMaJm5B1"// <- UPDATE
+                $0.clientKey =  "DDcvptPHMaGvWz6jTGSuBMlRXGSG2poVKdIDsre7"// <- UPDATE
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+            return true
     }
 
     // MARK: UISceneSession Lifecycle
